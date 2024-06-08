e79bae2a1bdf5ce11cc503fbb2c3e969a1624539
Skye J <skye@skyenet.online>
2024-06-07 21:26:00 -0400

    Ok but actually this time

--- src/Paperback/Paperback.ts
+++ src/Paperback/Paperback.ts
@@ -87,7 +87,7 @@ export class KomgaRequestInterceptor implements SourceInterceptor {
     async interceptRequest(request: Request): Promise<Request> {
         // Paper's Note: This hack no longer works on iOS 17
         // ORIGINAL NOTE: Doing it like this will make downloads work tried every other method did not work, if there is a better method make edit it and make pull request
-        console.log('Intercepting....');
+        console.log('Intercepting HERE....');
         if (request.url.includes('intercept*')) {
             console.log('Includes intercept');
             console.log(request.url);
@@ -101,7 +101,7 @@ export class KomgaRequestInterceptor implements SourceInterceptor {
         if (request.headers === undefined) {
             request.headers = {}
         }
-        console.log(JSON.stringify(request.headers, NULL, 4));
+        console.log(request.url);
         // We mustn't call this.getAuthorizationString() for the stateful submission request.
         // This procedure indeed catchs the request used to check user credentials
         // which can happen before an authorizationString is saved,
@@ -109,6 +109,8 @@ export class KomgaRequestInterceptor implements SourceInterceptor {
         // Thus we only inject an authorizationString if none are defined in the request
         if (request.headers.authorization === undefined) {
             request.headers.authorization = await getAuthorizationString(this.stateManager)
+        } else {
+          console.log(request.headers.authorization);
         }
         return request
     }
