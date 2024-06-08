8f87ca422e320c893c860c78ce3deb15cea2dcdc
Skye J <skye@skyenet.online>
2024-06-07 20:52:01 -0400

    Revert "Update Paperback.ts"
    This reverts commit 518e71a7a759a09b16dfc15aa6c665d080513649.

--- src/Paperback/Paperback.ts
+++ src/Paperback/Paperback.ts
@@ -89,7 +89,6 @@ export class KomgaRequestInterceptor implements SourceInterceptor {
         // Paper's Note: This hack no longer works on iOS 17
         // ORIGINAL NOTE: Doing it like this will make downloads work tried every other method did not work, if there is a better method make edit it and make pull request
         console.log('Intercepting....');
-        console.log(
         if (request.url.includes('intercept*')) {
             console.log('Includes intercept');
             console.log(request.url);
@@ -104,16 +103,7 @@ export class KomgaRequestInterceptor implements SourceInterceptor {
             request.headers = {}
         }
         else {
-            console.log(request    var out = '';
-    for (var i in obj) {
-        out += i + ": " + obj[i] + "\n";
-    }
-
-    alert(out);
-
-    // or, if you wanted to avoid alerts...
-
-    var pre = document.createElement('pre'););
+            console.log(request.url);
         }
         // We mustn't call this.getAuthorizationString() for the stateful submission request.
         // This procedure indeed catchs the request used to check user credentials
